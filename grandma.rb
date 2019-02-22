e4c599ef7580f287b77120677996c8ebae86202d
Alberto Morales <cmtmorales24@gmail.com>
2019-02-22 18:43:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(word)
-  if word==word.upcase
+  if word==word.upcase && word != "I LOVE YOU GRANDMA!"
     return" NO,NOT SINCE 1938!"
   
 elsif word=="I LOVE YOU GRANDMA!"
